c957861bcb66a7d392e205e492f294bb07f92b9b
TimTrev <s224681321@deakin.edu.au>
2025-05-23 14:13:46 +1000

    Delete ProjectPrototypeCode directory

--- ProjectPrototypeCode/secrets.h
+++
@@ -1,7 +0,0 @@
-// WiFi credentials
-const char WIFI_SSID[]     = "#####";    // Replace with Wi-Fi SSID
-const char WIFI_PASSWORD[] = "#####"; // Replace with Wi-Fi password
-
-// IFTTT Information
-#define IFTTT_EVENT "#####"   // Replace with IFTTT event name
-#define IFTTT_KEY "#####"  // Replace with IFTTT Key
